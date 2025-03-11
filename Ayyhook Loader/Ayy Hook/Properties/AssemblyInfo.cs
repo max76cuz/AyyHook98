b33694e099428ba50b12294466ee9bc21956b9c5
max76cuz <mkingston25@knox.nsw.edu.au>
2025-03-12 08:54:58 +1100

    Update AssemblyInfo.cs

--- Ayyhook Loader/Ayy Hook/Properties/AssemblyInfo.cs
+++ Ayyhook Loader/Ayy Hook/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Ayy Hook")]
+[assembly: AssemblyTitle("DrillV1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Ayy Hook")]
+[assembly: AssemblyProduct("DrillV1")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
