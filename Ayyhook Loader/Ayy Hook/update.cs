492ea4eb4dbb127bc814d8717cd4a38df33d6c3e
max76cuz <mkingston25@knox.nsw.edu.au>
2025-03-12 09:10:57 +1100

    Update update.cs

--- Ayyhook Loader/Ayy Hook/update.cs
+++ Ayyhook Loader/Ayy Hook/update.cs
@@ -1,4 +1,4 @@
-﻿// uses the the System namespace
+// uses the the System namespace
 using System;
 // uses the System.Windows.Forms namespace
 using System.Windows.Forms;
@@ -6,116 +6,66 @@ using System.Windows.Forms;
 using System.Net;
 // uses the System.Threading namespace
 using System.Threading;
-//(The using comments were given by Peatreat)
 
-//Why do I get the feeling you are going to be using this loader for a p2c? :thinking:
-
-//Also im too lazy to comment the rest of this loader so feel free to add your own and make a fork and ill merge it
-
-namespace Ayy_Hook
-//Open brackets
+namespace DrillV1
 {
     public partial class update : MetroFramework.Forms.MetroForm
-    //Open brackets
     {
         public update()
-        //Open brackets
         {
-            InitializeComponent(); //Kys
-        //Close brackets
+            InitializeComponent();
         }
 
-        string versioncheck = new WebClient().DownloadString(Settings.Check); //Gets the value of your version.txt and saves it as a string named versioncheck
+        string versioncheck = new WebClient().DownloadString(Settings.Check);
+        string location = System.IO.Path.GetDirectoryName(Application.ExecutablePath);
 
-        string location = System.IO.Path.GetDirectoryName(Application.ExecutablePath); //Gets the current dir/location of the loader and saves it as a string named location
-
-        private void update_Load(object sender, EventArgs e) //The code that is executed as soon as u open this form up
-        //Open brackets
+        private void update_Load(object sender, EventArgs e)
         {
-            //Just a littly shitty animation I made cause y not
-            metroLabel1.Text = "Loading."; //Changes the label to say "Loading."
-            //Thread.Sleep means it will take a quick nap
-            //I SWEAR TO FUCKING GOD IF YOU ASK ME WTF IS A NAP I WILL BAN YOU FROM THE DISCORD
-            //Note: every 1000 = 1 second
+            metroLabel1.Text = "Loading.";
             Thread.Sleep(100);
-            metroLabel1.Text = "Loading.."; //Changes the label to say "Loading.."
-            Thread.Sleep(100); //Thread.Sleep means it will take a quick nap
-            metroLabel1.Text = "Loading..."; //Changes the label to say "Loading..."
-            Thread.Sleep(100); //Thread.Sleep means it will take a quick nap
-            metroLabel1.Text = "Loading."; //Changes the label to say "Loading."
-            Thread.Sleep(100); //Thread.Sleep means it will take a quick nap
-            metroLabel1.Text = "Loading.."; //Changes the label to say "Loading.."
-            Thread.Sleep(100); //Thread.Sleep means it will take a quick nap
-            metroLabel1.Text = "Loading..."; //Changes the label to say "Loading..."
-            Thread.Sleep(100); //Thread.Sleep means it will take a quick nap
-            metroLabel1.Text = "Checking for update"; //Changes the label to say "Checking for update"
-            //End of animation
-            //Funfact: that small shitty animation I did plays too fast so u dont even see it when u open the loader
+            metroLabel1.Text = "Loading..";
+            Thread.Sleep(100);
+            metroLabel1.Text = "Loading...";
+            Thread.Sleep(100);
+            metroLabel1.Text = "Loading.";
+            Thread.Sleep(100);
+            metroLabel1.Text = "Loading..";
+            Thread.Sleep(100);
+            metroLabel1.Text = "Loading...";
+            Thread.Sleep(100);
+            metroLabel1.Text = "Checking for update";
 
-            if (versioncheck != Settings.version.ToString()) //Checks if url string is not equal to string in memory. -Peatreat
-            //Open brackets
+            if (versioncheck != Settings.version.ToString())
             {
-                timer1.Start(); //Starts timer1 so look at the code down below
-            //Close brackets
+                timer1.Start();
             }
-            else //If its not equal:
-            //Open brackets
+            else
             {
-                timer2.Start(); //Starts timer2 so look at the code down below
-            //Close brackets
+                timer2.Start();
             }
-        //Close brackets
         }
 
-        private void timer1_Tick(object sender, EventArgs e) //This is timer1 and if it was activated, well... code is below, this is self explanitory so kys
-        //Open brackets
+        private void timer1_Tick(object sender, EventArgs e)
         {
-            metroLabel1.Text = "Update found!"; //Changes the label to say "Update found!"
-            Thread.Sleep(100);//Thread.Sleep means it will take a quick nap
-            metroLabel1.Text = "Downloading"; //Changes the label to say "Downloading"
-            WebClient webClient = new WebClient(); //The thing used to connect to the worst place in the world...! The INTERNET!!! *DUN DUN DUNNNNNNN*
-            webClient.DownloadFile(Settings.Update, location); //Code to download "your" loader
-            metroLabel1.Text = "Done!"; //Changes the label to say "Done!"
-            Application.Exit(); //Close the loader after it downloaded the new loader
-            timer1.Stop(); //Stops the timer so this code ends and does not repeat. If you dont undertstand this, comment out this entire line and find out.
-        //Close brackets
+            metroLabel1.Text = "Update found!";
+            Thread.Sleep(100);
+            metroLabel1.Text = "Downloading";
+            WebClient webClient = new WebClient();
+            webClient.DownloadFile(Settings.Update, location);
+            metroLabel1.Text = "Done!";
+            Application.Exit();
+            timer1.Stop();
         }
 
         private void timer2_Tick(object sender, EventArgs e)
-        //Open brackets
         {
-            metroLabel1.Text = "No update found :)"; //Changes the label to say "No update found :)"
-            Thread.Sleep(1000); //Thread.Sleep means it will take a quick nap for 1 sec (Litterly)
-            //---------------------------------------------------------------------------------------
-            //All this does is just closes this form and opens form2. so lemme break it down:
-            //Makes a variable called form2 which equals to open form2
+            metroLabel1.Text = "No update found :)";
+            Thread.Sleep(1000);
             var form1 = new Form1();
-            //Overall its just a tricky way to not have to declare an entire function (event handler) 
-            //outside of the current one that handles Form2.Closed event. – KDecker 
-            //Also note: I didnt really know how to explain this but this guy did a good job!
             form1.Closed += (s, args) => this.Close();
-            //This shows form2
             form1.Show();
-            //This closes/hides this form
             this.Hide();
-            //---------------------------------------------------------------------------------------
-            timer2.Stop();//Stops the timer so this code ends and does not repeat. If you dont undertstand this, comment out this entire line and find out.
-        //Close brackets
+            timer2.Stop();
         }
-    //Close brackets
     }
-//Close brackets
 }
-
-//-----------------------------------------------------
-// Coded by /id/Roshly! Ayyhook loader source
-// https://github.com/Roshly/AyyHook-Loader
-// Note to the person using this, removing this
-// text is in violation of the license you agreed
-// to by downloading. Only you can see this so what
-// does it matter anyways.
-// Copyright © Roshly 2018
-// Licensed under a MIT license
-// Read the terms of the license here
-// https://github.com/Roshly/AyyHook-Loader/blob/master/LICENSE
-//-----------------------------------------------------
